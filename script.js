2aa6e30022dc45cfeec00b65075b8012e80f27b2
jamshid <jamanegmatulin@gmail.com>
2020-02-09 23:01:28 +0500

    -js ls1

---
+++ script.js
@@ -0,0 +1,24 @@
+var userName = prompt('Введите своё имя');
+console.log('Ваше имя ' + userName);
+
+var age = prompt('Введите свой возраст');
+console.log('Ваш возраст ' + age);
+
+var num = prompt('Решите пример 6 + 3 = ?');
+console.log('6 + 3 = 9 Ваш ответ ' + num);
+
+var num1 = prompt('Решите пример 20 - 5 = ?');
+console.log('20 - 5 = 15 Ваш ответ ' + num1);
+
+var num2 = prompt('Решите пример 15 * 2 = ?');
+console.log('15 * 2 = 30 Ваш ответ ' + num2);
+
+var num3 = prompt('Решите пример 8 / 4 = ?');
+console.log('8 / 4 = 2 Ваш ответ ' + num3);
+
+var num4 = prompt('Решите пример 10 % 3 = ?');
+console.log('10 % 3 = 1 Ваш ответ ' + num4);
+
+alert('Откройте консоль для просмотра результата')
+
+
